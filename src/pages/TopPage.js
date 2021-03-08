335d37de4b634fedae58a08cd6444f4757cb5317
Rei Sugiura <reibomaru@gmail.com>
2021-03-08 11:21:51 +0900

    feat: top pageにサイトの説明を追加

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function AboutPage(){
     return(
-        <div>
-            
-        </div>
+        <>
+        <h1>this is top</h1>
+        <h2>このサイトについて</h2>
+        <p>このサイトはReactをサイトを作りながら学習するサイトです。</p>
+        <h2>目次</h2>
+            <ul>
+                <li><Link to="/todo">todo</Link>: functional componentを使ってReactの基礎的な知識(component, props, JSX)で作成されたtodo app</li>
+                <li><Link to="/about">about</Link>: 参考リンク(React Routerを勉強する際にできたページ)</li>
+                <li><Link to="/count">count</Link>: React Contextを使って作成されたcount up app</li>
+            </ul>
+        </>
     )
 }
 
